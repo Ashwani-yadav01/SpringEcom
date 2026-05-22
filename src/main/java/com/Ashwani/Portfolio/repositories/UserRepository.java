7c55ee84f7a71e60d8e7d2f07136240a00c908a5
Ashwani-yadav01 <yadavashwaniyadav273209@gmail.com>
2026-05-22 22:55:54 +0530

    auths

---
+++ src/main/java/com/Ashwani/Portfolio/repositories/UserRepository.java
@@ -0,0 +1,12 @@
+package com.Ashwani.Portfolio.repositories;
+
+import com.Ashwani.Portfolio.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User ,Long> {
+    Optional<User> findByUsername(String username);
+}
--- src/main/java/com/Ashwani/Portfolio/security/request/LoginRequest.java
+++ src/main/java/com/Ashwani/Portfolio/security/request/LoginRequest.java
@@ -1,9 +1,12 @@
-package com.Ashwani.Portfolio.security.jwt;
+package com.Ashwani.Portfolio.security.request;
 
+import jakarta.validation.constraints.NotBlank;
 
 public class LoginRequest {
+    @NotBlank
     private String username;
 
+    @NotBlank
     private String password;
 
     public String getUsername() {
